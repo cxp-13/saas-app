c44c299f998faa758328c6b68908fc9ed195995e
cxp-13 <lantianlaoli@gmail.com>
2025-06-07 18:00:43 +0800

    Add middleware for Clerk authentication, update dependencies, and implement companion features

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS </h1>
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={10}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths "
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={3}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList companions={recentSessions} />
+        <CTA />
+
+      </section>
+
+    </main>
   )
 }
 
---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import NavItems from './NavItems'
+import {
+    ClerkProvider,
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from '@clerk/nextjs'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <Link href='/'>
+                <div className='flex items-center'>
+                    <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
+                </div>
+            </Link>
+            <div className='flex items-center gap-8'>
+                <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className='btn-signin'>Sign in</button>
+                    </SignInButton>
+                </ SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </ SignedIn>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
